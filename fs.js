79fd976c3b44117a323793d5f458259ac632d688
kumar <santanupaikaray23@gmail.com>
2021-05-21 16:55:38 +0530

    add all file

--- fs.js
+++ fs.js
@@ -9,7 +9,15 @@ var fs=require ('fs')
 //     if(error)throw error;
 //     console.log("File Appended")
 // })
-fs.readFile('db.json','utf-8',function(err,data){
-    if(err) throw err;
-    console.log(data)
+// fs.readFile('db.json','utf-8',function(err,data){
+//     if(err) throw err;
+//     console.log(data)
+// })
+// fs.rename('myCode.txt','First.txt',function(err){
+//     if(err)throw err;
+//     console.log('File rename')
+// })
+fs.unlink("First.txt",function(err){
+     if(err) throw err;
+    console.log('File Deleted')
 })
---
+++ server.js
@@ -0,0 +1,6 @@
+var http = require('http')
+var server = http.createServer(function(req,res){
+    res.write('<h1>Create Node Server</h1>')
+    res.end()
+})
+server.listen(8800)
